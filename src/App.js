a0905d3c874e99c4eace390f3abf20298b6a5a4c
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-30 03:12:00 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 const App = () => {
     return (
-        <div>
-            Meaddb
+        <div className='overflow-hidden'>
+            <Header></Header>
+            <Routes>
+                <Route></Route>
+            </Routes>
         </div>
     );
 };
