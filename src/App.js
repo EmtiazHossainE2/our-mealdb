8dfb26626d8b6875b8db28106eb6480d2d29fc4b
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-30 14:56:08 +0600

    header responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './Pages/Home/Home';
+import Restaurant from './Pages/Restaurant/Restaurant';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
 
 const App = () => {
     return (
         <div className='overflow-hidden'>
-            <h3 className='text-center bg-danger'>Test</h3>
-
+            <Header></Header>
+            <Routes>
+                <Route path='/' element={<Home></Home>}></Route>
+                <Route path='/restaurant' element={<Restaurant></Restaurant>}></Route>
+                <Route path='/about' element={<About></About>}></Route>
+                <Route path='/contact' element={<Contact></Contact>}></Route>
+            </Routes>
         </div>
     );
 };
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css'
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import CustomLink from '../CustomLink/CustomLink';
+import logo from '../../logo.webp'
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container className='pb-3'>
+                    <Navbar.Brand href="#home"><img src={logo} alt="" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" className='nav-end'>
+                        <Nav className='nav-style pb-2'>
+                            <CustomLink to='/'>Home</CustomLink>
+                            <CustomLink to='/restaurant'>Restaurant</CustomLink>
+                            <CustomLink to='/about'>About Us</CustomLink>
+                            <CustomLink to='/contact'>Contact Us</CustomLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
