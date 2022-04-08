01466759a3b8807974a69a865ea1a3e6438f3b59
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-08 20:25:14 +0600

    email password auth check

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,15 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AuthForm.css";
 import GoogleLogo from "../../images/google.svg";
 import { useNavigate } from "react-router-dom";
 import app from "../../firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
 const auth = getAuth(app)
 
 const Login = () => {
     const navigate = useNavigate();
+    const [email, setEmail] = useState({ value: "", error: "" });
+    const [password, setPassword] = useState({ value: "", error: "" })
 
+
+    //handle email  
+    const handleEmail = (e) => {
+        // console.log( e.target.value);
+        const emailValue = e.target.value
+        if (/\S+@\S+\.\S+/.test(emailValue)) {
+            setEmail({ value: emailValue, error: "" });
+        } else {
+            setEmail({ value: "", error: "Please Provide a valid Email" });
+        }
+    }
+
+    //handle password  
+    const handlePassword = e => {
+        // console.log(e.target.value);
+        const passwordValue = e.target.value
+        setPassword({ value: passwordValue, error: "" });
+    }
+
+    // handle login 
+    const handleLogIn = e => {
+        e.preventDefault();
+
+        if (email.value === "") {
+            setEmail({ value: "", error: "Email is required" });
+        }
+
+        if (password.value === "") {
+            setPassword({ value: "", error: "Password is required" });
+        }
+        if (email.value && password.value) {
+            signInWithEmailAndPassword(auth, email.value, password.value)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log(user);
+                    navigate("/");
+                })
+                .catch((error) => {
+                    const errorMessage = error.message;
+
+                    if (errorMessage.includes("wrong-password")) {
+                        console.error("Wrong Password", { id: "error" });
+                    } else {
+                        console.error(errorMessage, { id: "error" });
+                    }
+                });
+        }
+
+
+    }
 
     //google provider handle 
     const handleGoogleSignIn = () => {
@@ -30,20 +82,20 @@ const Login = () => {
         <div className='auth-form-container '>
             <div className='auth-form'>
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={handleLogIn}>
                     <div className='input-field'>
                         <label htmlFor='email'>Email</label>
-                        <div className='input-wrapper'>
+                        <div className='input-wrapper' onBlur={handleEmail}>
                             <input type='text' name='email' id='email' />
                         </div>
                     </div>
                     <div className='input-field'>
                         <label htmlFor='password'>Password</label>
-                        <div className='input-wrapper'>
+                        <div className='input-wrapper' onBlur={handlePassword}>
                             <input type='password' name='password' id='password' />
                         </div>
                     </div>
-                    <button type='submit' className='auth-form-submit'>
+                    <button type='submit' className='auth-form-submit' >
                         Login
                     </button>
                 </form>
